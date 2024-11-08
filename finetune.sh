c0ca7210350b31483fb42e7dc81f437dcd829216
Ian Ross <ian.ackerman.ross@gmail.com>
2024-11-08 19:41:57 +0000

    Pass in uuid

--- finetune.sh
+++ finetune.sh
@@ -2,13 +2,19 @@
 
 if [ $# -eq 0 ]
 then
-    epochs=25
+    echo "No arguments supplied. Exiting."
+    exit 1
 else
     epochs=$1
+    run_uuid=$2
 fi
+
+hostname
+echo "Running with max_epochs: $epochs"
 
 ln -s /workspace/metl/data/
 ln -s /workspace/metl/pretrained_models/
 
-python /workspace/metl/code/train_target_model.py @/workspace/metl/args/finetune_avgfp_local.txt --enable_progress_bar false --enable_simple_progress_messages --max_epochs $epochs --unfreeze_backbone_at_epoch 25
+
+python /workspace/metl/code/train_target_model.py @/workspace/metl/args/finetune_avgfp_local.txt --enable_progress_bar false --enable_simple_progress_messages --max_epochs $epochs --uuid=$run_uuid --unfreeze_backbone_at_epoch 25
 
