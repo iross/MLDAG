92ae0449d29bab91e87cd9988fe3100a3c437d92
Ian Ross <ian.ackerman.ross@gmail.com>
2024-12-11 18:56:22 +0000

    A bit of formatting for readability

--- finetune.sh
+++ finetune.sh
@@ -16,5 +16,10 @@ ln -s /workspace/metl/data/
 ln -s /workspace/metl/pretrained_models/
 
 
-python /workspace/metl/code/train_target_model.py @/workspace/metl/args/finetune_avgfp_local.txt --enable_progress_bar false --enable_simple_progress_messages --max_epochs $epochs --uuid=$run_uuid --unfreeze_backbone_at_epoch 25
+python /workspace/metl/code/train_target_model.py \
+    @/workspace/metl/args/finetune_avgfp_local.txt \
+    --enable_progress_bar false \
+    --enable_simple_progress_messages \
+    --unfreeze_backbone_at_epoch 25 \
+    --max_epochs $epochs --uuid=$run_uuid 
 
