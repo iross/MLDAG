b0f51c3b7ec0a6b40e5abafcddde32b3361fc652
Ian Ross <iaross@wisc.edu>
2025-03-13 10:58:31 -0500

    Switch to global

--- pretrain.sh
+++ pretrain.sh
@@ -8,14 +8,17 @@ else
     run_uuid=$2
 fi
 
-#cp /staging/iaross/cleaned_data_test.zip .
+#echo "Copying global dataset"
+# cp /staging/iaross/processed-global.tar.gz .
+echo "Untarring global dataset"
+tar xzvf processed-global.tar.gz
 
-unzip cleaned_data_test.zip -d precleaned
-rm cleaned_data_test.zip
+#unzip cleaned_data_test.zip -d precleaned
+rm processed-global.tar.gz
 
 ln -s /workspace/metl/data/
 
 python /workspace/metl/code/train_source_model.py @/workspace/metl/args/pretrain_avgfp_local.txt \
-    --ds_fn $PWD/precleaned/data/rosetta_data/avgfp/avgfp.db   \
-    --split_dir $PWD/precleaned/data/rosetta_data/avgfp/splits/standard_tr0.8_tu0.1_te0.1_w5fb1ed670b87_r4991 \
+    --ds_fn $PWD/global/global.db   \
+    --split_dir $PWD/global/splits/standard_tr0.9_tu0.05_te0.05_w2a93d88bac32_r2098 \
     --max_epochs $epochs --uuid=$run_uuid 
