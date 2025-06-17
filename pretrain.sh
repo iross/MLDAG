473fff9f621f4f408fb911058b95528eddc43831
Ian Ross <iross@cs.wisc.edu>
2025-06-17 11:28:48 -0500

    update wrapper script

--- pretrain.sh
+++ pretrain.sh
@@ -6,7 +6,9 @@ then
 else
     epochs=$1
     run_uuid=$2
+    random_seed=$3
 fi
+nvidia-smi
 
 #echo "Copying global dataset"
 # cp /staging/iaross/processed-global.tar.gz .
@@ -18,7 +20,31 @@ rm processed-global.tar.gz
 
 ln -s /workspace/metl/data/
 
+pwd
+env
+
+mkdir wandb
+mkdir wandb_data
+export WANDB_DIR=$PWD/wandb
+export WANDB_DATA_DIR=$PWD/wandb_data
+export WANDB_CACHE_DIR=$PWD/wandb/.cache
+export WANDB_CONFIG_DIR=$PWD/wandb/.config
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
 python /workspace/metl/code/train_source_model.py @/workspace/metl/args/pretrain_avgfp_local.txt \
     --ds_fn $PWD/global/global.db   \
     --split_dir $PWD/global/splits/standard_tr0.9_tu0.05_te0.05_w2a93d88bac32_r2098 \
-    --max_epochs $epochs --uuid=$run_uuid 
+    --max_epochs $epochs --uuid=$run_uuid  \
+    --condor_checkpoint_every_n_epochs 1 \
+    --use_wandb \
+    --wandb_online --wandb_project metl_global_$run_uuid \
+    --random_seed $random_seed
+
+#python /workspace/metl/code/train_source_model.py @/workspace/metl/args/pretrain_avgfp_local.txt \
+    #--ds_fn $PWD/global/global.db   \
+    #--split_dir $PWD/global/splits/standard_tr0.9_tu0.05_te0.05_w2a93d88bac32_r2098 \
+    #--max_epochs $epochs --uuid=$run_uuid  \
+    #--condor_checkpoint_every_n_epochs 1 \
+    ##--use_wandb \
+    ##--wandb_online --wandb_project metl_global_$run_uuid \
+    #--random_seed $random_seed
